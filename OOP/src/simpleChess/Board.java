2170e5cfbd5bd3364768c7f6dc0bb9aea7f4de1b
torst <torst@DESKTOP-51DTKK2>
2018-06-08 17:02:36 +0200

    ggt

--- OOP/src/simpleChess/Board.java
+++ OOP/src/simpleChess/Board.java
@@ -5,30 +5,94 @@ import javax.swing.event.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class Board extends JFrame {
 
 	private Color colorBlack = Color.BLACK;
 	public JButton[][] buttons = new JButton[8][8];
-	public Figure[] figures = new Figure[32];?
+	public Figure[] figures = new Figure[32];
 	public ButtonListener buttonListener = new ButtonListener(this);
 	public String rights;
 	public int[][] pawnMoves;
-	// public Figure figure = figures[0].getFigure();
-	// public boolean canMoveToBauer(dRow, dCol) {
-	// mögliche felder von origin bis destination aufdecken, für jedes feld
-	// entscheiden
-	// }button figur übergeben?
-	//button setzt dann sich selbst icon von der figur die es hat
-	// schach: könig suchen, felder rumherum aufdecken bis figur, vojn figuren typ
-	// prüfen ob schach
+	public int originRow;
+	public int originCol;
+	public int destRow;
+	public int destCol;
 
 	public void processRightClick(int row, int col) {
+		originRow = row;
+		originCol = col;
+		int index = getFigureIndex(row, col);
+		Figure figure = getFig(index);
+		pawnMoves = figure.getMoves();
 
 	}
 
-	public void processLeftClick(int row, int col) {
-		System.out.println(row + " " + col);
+	public Figure getFig(int index) {
+		return figures[index];
+	}
+
+	public void processLeftClick(int row, int col) { 
+		destRow = row;
+		destCol = col;
+		System.out.println(canMove());
+		if (canMove()) {
+			move();
+		}
+	}
+
+	public Integer getFigureIndex(int row, int col) {
+
+		for (int g = 0; g < figures.length; g++) {
+			if (figures[g] != null) {
+				if (figures[g].row == row && figures[g].col == col) {
+					return g;
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean canMove() {
+		for (int i = 0; i <= destRow; i++) {
+
+			int row = pawnMoves[i][0];
+			int col = pawnMoves[i][1];
+			System.out.println(row + " " + col);
+			int figureIndex = getFigureIndex(row, col);
+			Figure fig = figures[figureIndex];
+			if (fig.icon != null) {
+				return false;
+			}
+
+		}
+		return true;
+	}
+
+	public void move() {
+		int index = getFigureIndex(originRow, originCol);
+		figures[index].row = destRow;
+		figures[index].col = destCol;
+
+		int destIndex = getFigureIndex(destRow, destCol);
+
+		if (figures[destIndex] != null) {
+			figures[destIndex].row = null;
+			figures[destIndex].col = null;
+		}
+
+	}
+
+	public void repaint() {
+
+		for (int i = 0; i < 32; i++) {
+			if (figures[i] != null) {
+				int row = figures[i].row;
+				int col = figures[i].col;
+				buttons[row][col].setIcon(figures[i].icon);
+			}
+		}
 	}
 
 	public Board() {
@@ -46,11 +110,22 @@ public class Board extends JFrame {
 			}
 
 		}
+
 		setSize(500, 500);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Figure figure = new Figure(new ImageIcon("Images\\turmBlack.png"), "white", "bauer", 0, 4);
+		Figure figure2 = new Figure(new ImageIcon("Images\\turmWhite.png"), "white", "bauer", 0, 0);
+		figures[0] = figure;
+		figures[1] = figure2;
+
+		repaint();
+
+		System.out.println(Arrays.deepToString(figures[0].getMoves()));
+		System.out.println(Arrays.deepToString(figures[1].getMoves()));
+
 	}
 
 	public static void main(String[] args) {
--- OOP/src/simpleChess/Figure.java
+++ OOP/src/simpleChess/Figure.java
@@ -6,21 +6,63 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Figure {
-	public Figure getFigure() {
-		return this;
-	}
+
 	ImageIcon icon;
 	String type;
 	String player;
-	public int row;
-	public int col;
+	public Integer row;
+	public Integer col;
 	boolean hasMoved;
 
-	public int[][] getMovesBauer() {
-		return null;
+	public int[][] moves = new int[14][2];
+
+	public int[][] getMoves() {
+		int x = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (isValidMove(i, j)) {
+
+					moves[x][0] = i;
+					moves[x][1] = j;
+					x++;
+					if (x == 14) {
+						return moves;
+					}
+
+				}
+			}
+		}
+		return moves;
+	}
+
+	private boolean isValidMove(int i, int j) {
+
+		int rowDelta = Math.abs(i - row);
+		int colDelta = Math.abs(j - col);
+
+		if ((rowDelta == 0 && colDelta > 0)) {
+			return true;
+		}
+		if ((colDelta == 0 && rowDelta > 0)) {
+			return true;
+		}
+		return false;
+
 	}
 
 	public int[][] getMoveKönig() {
 		return null;
+	}
+
+	public Figure(ImageIcon icon, String player, String type, Integer row, Integer col) {
+		this.icon = icon;
+		this.player = player;
+		this.type = type;
+		this.row = row;
+		this.col = col;
+	}
+	public Figure(Integer row, Integer col) {
+		this.row = row;
+		this.col = col;
 	}
 }
