1f1426beee8151c0091f52c926458d1ce623f1ef
torst <torst@DESKTOP-51DTKK2.home>
2018-06-28 13:21:02 +0200

    j

--- OOP/src/minesweeper/Field.java
+++ OOP/src/minesweeper/Field.java
@@ -8,14 +8,7 @@ public class Field extends JButton {
 	private int minesAround;
 	private boolean bomb = false;
 	private boolean flagged = false;
-
-	public boolean getFlagged() {
-		return flagged;
-	}
-
-	public void setFlagged(boolean flagged) {
-		this.flagged = true;
-	}
+	private boolean hidden = true;
 
 	public int getMinesAround() {
 		return minesAround;
@@ -25,12 +18,31 @@ public class Field extends JButton {
 		this.minesAround = minesAround;
 	}
 
-	public void setBomb(boolean bomb) {
-		this.bomb = bomb;
+	public void setBomb() {
+		this.bomb = true;
 	}
 
-	public boolean getBomb() {
+	public boolean hasBomb() {
 		return this.bomb;
 	}
 
+	public boolean isFlagged() {
+		return flagged;
+	}
+
+	public void flag() {
+		this.flagged = true;
+	}
+
+	public void unflag() {
+		this.flagged = false;
+	}
+
+	public void unhide() {
+		this.hidden = false;
+	}
+
+	public boolean isHidden() {
+		return hidden;
+	}
 }
--- OOP/src/minesweeper/Game.java
+++ OOP/src/minesweeper/Game.java
@@ -8,8 +8,20 @@ import java.util.Random;
 
 public class Game extends JFrame {
 
-	private final Field[][] fields = new Field[9][9];
+	private Field[][] fields = new Field[9][9];
 	private ImageIcon[] icons = new ImageIcon[12];
+	long tStart = 0;
+
+	private void setTimer() {
+		tStart = System.currentTimeMillis();
+	}
+
+	private void cancelTimer() {
+		long tEnd = System.currentTimeMillis();
+		long tDelta = tEnd - tStart;
+		double elapsedSeconds = tDelta / 1000.0;
+		System.out.println(elapsedSeconds);
+	}
 
 	private void initImages() {
 		for (int i = 0; i < 9; i++) {
@@ -25,13 +37,13 @@ public class Game extends JFrame {
 		Random zufall = new Random();
 
 		for (int i = 0; i < 10; i++) {
-			int randomWidth = zufall.nextInt(9);
-			int randomHeight = zufall.nextInt(9);
+			int row = zufall.nextInt(9);
+			int col = zufall.nextInt(9);
 
-			if (fields[randomWidth][randomHeight].getBomb()) {
+			if (fields[row][col].hasBomb()) {
 				i--;
 			} else {
-				fields[randomWidth][randomHeight].setBomb(true);
+				fields[row][col].setBomb();
 			}
 		}
 	}
@@ -41,45 +53,45 @@ public class Game extends JFrame {
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
 				int counter = 0;
-				if (fields[i][j].getBomb() == false) {
+				if (fields[i][j].hasBomb() == false) {
 
 					if (i != 8) {
-						if (fields[i + 1][j].getBomb()) {
+						if (fields[i + 1][j].hasBomb()) {
 							counter++;
 						}
 					}
 					if (i != 0) {
-						if (fields[i - 1][j].getBomb()) {
+						if (fields[i - 1][j].hasBomb()) {
 							counter++;
 						}
 					}
 					if (j != 8) {
-						if (fields[i][j + 1].getBomb()) {
+						if (fields[i][j + 1].hasBomb()) {
 							counter++;
 						}
 					}
 					if (j != 0) {
-						if (fields[i][j - 1].getBomb()) {
+						if (fields[i][j - 1].hasBomb()) {
 							counter++;
 						}
 					}
 					if (i != 8 && j != 8) {
-						if (fields[i + 1][j + 1].getBomb()) {
+						if (fields[i + 1][j + 1].hasBomb()) {
 							counter++;
 						}
 					}
 					if (i != 0 && j != 0) {
-						if (fields[i - 1][j - 1].getBomb()) {
+						if (fields[i - 1][j - 1].hasBomb()) {
 							counter++;
 						}
 					}
 					if (i != 8 && j != 0) {
-						if (fields[i + 1][j - 1].getBomb()) {
+						if (fields[i + 1][j - 1].hasBomb()) {
 							counter++;
 						}
 					}
 					if (i != 0 && j != 8) {
-						if (fields[i - 1][j + 1].getBomb()) {
+						if (fields[i - 1][j + 1].hasBomb()) {
 							counter++;
 						}
 					}
@@ -91,93 +103,77 @@ public class Game extends JFrame {
 	}
 
 	private void uncoverAroundCell(int row, int col) {
-
 		if (row != 8) {
-			if (fields[row + 1][col].getBomb() == false) {
-				int mines = fields[row + 1][col].getMinesAround();
-				fields[row + 1][col].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row + 1, col);
-				}
+			if (fields[row + 1][col].isHidden()) {
+				uncover(row + 1, col);
 			}
 		}
 		if (row != 0) {
-			if (fields[row - 1][col].getBomb() == false) {
-				int mines = fields[row - 1][col].getMinesAround();
-				fields[row - 1][col].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row - 1, col);
-				}
+			if (fields[row - 1][col].isHidden()) {
+				uncover(row - 1, col);
 			}
 		}
 		if (col != 8) {
-			if (fields[row][col + 1].getBomb() == false) {
-				int mines = fields[row][col + 1].getMinesAround();
-				fields[row][col + 1].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row, col + 1);
-				}
+			if (fields[row][col + 1].isHidden()) {
+				uncover(row, col + 1);
 			}
 		}
 		if (col != 0) {
-			if (fields[row][col - 1].getBomb() == false) {
-				int mines = fields[row][col - 1].getMinesAround();
-				fields[row][col - 1].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row, col - 1);
-				}
+			if (fields[row][col - 1].isHidden()) {
+				uncover(row, col - 1);
 			}
 		}
 		if (row != 8 && col != 8) {
-			if (fields[row + 1][col + 1].getBomb() == false) {
-				int mines = fields[row + 1][col + 1].getMinesAround();
-				fields[row + 1][col + 1].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row + 1, col + 1);
-				}
+			if (fields[row + 1][col + 1].isHidden()) {
+				uncover(row + 1, col + 1);
 			}
 		}
 		if (row != 0 && col != 0) {
-			if (fields[row - 1][col - 1].getBomb() == false) {
-				int mines = fields[row - 1][col - 1].getMinesAround();
-				fields[row - 1][col - 1].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row - 1, col - 1);
-				}
+			if (fields[row - 1][col - 1].isHidden()) {
+				uncover(row - 1, col - 1);
 			}
 		}
 		if (row != 8 && col != 0) {
-			if (fields[row + 1][col - 1].getBomb() == false) {
-				int mines = fields[row + 1][col - 1].getMinesAround();
-				fields[row + 1][col - 1].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row + 1, col - 1);
-				}
+			if (fields[row + 1][col - 1].isHidden()) {
+				uncover(row + 1, col - 1);
 			}
 		}
 		if (row != 0 && col != 8) {
-			if (fields[row - 1][col + 1].getBomb() == false) {
-				int mines = fields[row - 1][col + 1].getMinesAround();
-				fields[row - 1][col + 1].setIcon(icons[mines]);
-				if (mines == 0) {
-					uncoverAroundCell(row - 1, col + 1);
-				}
+			if (fields[row - 1][col + 1].isHidden()) {
+				uncover(row - 1, col + 1);
 			}
 		}
 	}
 
 	private void uncover(int row, int col) {
+		Field field = fields[row][col];
+		if (field.hasBomb()) {
 
-		if (fields[row][col].getBomb()) {
-			fields[row][col].setIcon(icons[9]);
+			System.exit(0);
 		} else {
-			fields[row][col].setIcon(icons[fields[row][col].getMinesAround()]);
-
-			if (fields[row][col].getMinesAround() == 0) {
+			int mines = field.getMinesAround();
+			field.setIcon(icons[mines]);
+			field.unhide();
+			if (mines == 0) {
 				uncoverAroundCell(row, col);
 			}
 		}
 
+	}
+
+	private void checkWin() {
+		int counter = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (fields[i][j].isFlagged() && fields[i][j].hasBomb()) {
+					counter++;
+				}
+			}
+		}
+		if (counter == 10) {
+			System.out.println("won");
+			cancelTimer();
+		}
 	}
 
 	private void newGame() {
@@ -211,15 +207,40 @@ public class Game extends JFrame {
 	private class ClickHandler extends MouseAdapter {
 		public void mousePressed(MouseEvent e) {
 			Object source = e.getSource();
+			Field field = null;
+			int row = 0;
+			int col = 0;
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9; j++) {
 					if (source == fields[i][j]) {
-						uncover(i, j);
-						return;
+						row = i;
+						col = j;
+						field = fields[row][col];
+					}
+				}
+			}
+			if (e.getButton() == MouseEvent.BUTTON3) {
+				if (field.isHidden()) {
+					if (field.isFlagged()) {
+						field.unflag();
+						field.setIcon(icons[10]);
+					} else {
+						field.flag();
+						field.setIcon(icons[11]);
 					}
 				}
 
+			} else {
+				if (tStart == 0) {
+					setTimer();
+				}
+				if (field.isFlagged()) {
+					return;
+				} else {
+					uncover(row, col);
+				}
 			}
+			checkWin();
 		}
 	}
 
