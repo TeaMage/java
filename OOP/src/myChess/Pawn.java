5fe96b6e73df40c33779403e3c9c57e7ac51ce6f
torst <torst@DESKTOP-51DTKK2.home>
2018-06-29 12:03:16 +0200

    ff

--- OOP/src/myChess/Pawn.java
+++ OOP/src/myChess/Pawn.java
@@ -7,11 +7,21 @@ public class Pawn extends Figure {
 
 	public void fillMoves(boolean player) {
 		if (player) {
+			if (!hasMoved) {
+				
+				if(row-1)
+				
+				for(int i = 0; i < 32; i++) {
+					figures[i].getRow();
+					figures[i].getCol();
+				}
+			}
+		} else {
 
 		}
 	}
 
-	public Pawn(int row, int col, boolean player, ImageIcon icon,Figure[] figures) {
-		super(row, col, player, icon,figures);
+	public Pawn(int row, int col, boolean player, ImageIcon icon, Figure[] figures) {
+		super(row, col, player, icon, figures);
 	}
 }
